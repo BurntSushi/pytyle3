c84aba68fcd6097c073e1eb23bfa652e0571e68f
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-02-26 16:24:55 -0500

    added configuration option and explanation for margins

--- config.py
+++ config.py
@@ -6,6 +6,22 @@ ignore = ['gmrun', 'krunner']
 # How much to increment the master area proportion size
 proportion_change = 0.02
 
+# If you have panels that don't set struts (*ahem* JWM's panel), then
+# setting a margin is the only way to force pytyle not to cover your panels.
+# IMPORTANT NOTE: If you set *any* margin, pytyle3 will automatically skip
+# all strut auto-detection. So your margins should account for all panels, even
+# if the others set struts.
+#
+# The format here is to have one set of margins for each active physical
+# head. They should be in the following order: Left to right, top to bottom.
+# Make sure to set 'use_margins' to True!
+use_margins = False
+margins = [
+    {'top': 50, 'bottom': 50, 'left': 50, 'right': 50},
+    {'top': 50, 'bottom': 50, 'left': 50, 'right': 50},
+    {'top': 50, 'bottom': 50, 'left': 50, 'right': 50},
+]
+
 # Whether to send any debug information to stdout
 debug = False
 
