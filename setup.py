cea802dd2fb77da710a6cdfe65be9e8e211b8de7
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-07-05 15:16:54 -0400

    Updated wrong references to GPL in setup.py to WTFPL.

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-#==============================================================================
-# pytyle3 - An incomplete xcb-util port plus some extras
-# Copyright (C) 2009-2010  Andrew Gallant <andrew@pytyle.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#==============================================================================
-
 import os
 import os.path
 import sys
@@ -37,7 +18,7 @@ setup(
     author = 'Andrew Gallant',
     author_email = 'andrew@pytyle.com',
     version = '3.0.0',
-    license = 'GPL',
+    license = 'WTFPL',
     description = 'A new and much more lightweight pytyle that supports Openbox Multihead',
     long_description = 'See README',
     url = 'https://github.com/BurntSushi/pytyle3',
